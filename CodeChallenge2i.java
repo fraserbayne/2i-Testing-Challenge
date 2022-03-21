4c788cbce594b09cbbe4cfad8e6134a43eee3c74
fraserbayne <67072629+fraserbayne@users.noreply.github.com>
2022-03-21 16:23:02 +0000

    Added assumptions made

--- CodeChallenge2i.java
+++ CodeChallenge2i.java
@@ -2,6 +2,23 @@ public class CodeChallenge2i {
 
 	public static void main(String[] args) {
 
+		/*
+		 * ASSUMPTIONS:
+		 * 
+		 * A>0
+		 * 
+		 * X>=0
+		 * 
+		 * Iteration starts at 0 but does not perform any function until the first valid
+		 * multiple of A is reached
+		 * 
+		 * Iterates until the next iteration would exceed the bounds specified but does
+		 * but not pass them
+		 * 
+		 * Once each multiple of X is reached, iteration is continued from the last
+		 * value incremented to up until the next multiple of X. Instead of continuing
+		 * from the previous multiple of X.
+		 */
 		int A = 20;
 		int X = 40;
 		int index = 0;
